368086aa9c1f7ad4de4e975c694d368033024289
Tharushi Kawodya Arachchige <139765472+kawodyaarachchige@users.noreply.github.com>
2024-07-09 03:34:32 +0530

    updated home section

--- script.js
+++ script.js
@@ -1,33 +1,12 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const accordionButtons = document.querySelectorAll(".accordion-button");
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
 
-    accordionButtons.forEach(button => {
-        button.addEventListener("click", () => {
-            const content = button.nextElementSibling;
-
-            // Close other open accordion contents
-            document.querySelectorAll('.accordion-content').forEach(item => {
-                if (item !== content) {
-                    item.style.display = 'none';
-                }
-            });
-
-            // Toggle display of the current content
-            if (content.style.display === "block") {
-                content.style.display = "none";
-            } else {
-                content.style.display = "block";
-            }
-        });
-    });
-
-    // Close accordion content if click happens outside
-    window.addEventListener('click', function(e) {
-        if (!e.target.matches('.accordion-button')) {
-            document.querySelectorAll('.accordion-content').forEach(content => {
-                content.style.display = 'none';
-            });
-        }
-    });
 });
 
+function searchFunction() {
+    let query = document.querySelector('.search-input').value;
+    window.location.href = `https://www.google.com/search?q=${query}`;
+}
